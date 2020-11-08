5e4a9b60512e7d39e490cfebf988440cc6b900a7
CICSilver <1038516368@qq.com>
2020-11-08 02:32:16 +0000

    optimized find_root to O(1)

--- union-find/union_find.c
+++ union-find/union_find.c
@@ -6,8 +6,10 @@
 void union_init(int size)
 {
     parent = (int *)calloc(size, sizeof(int));
+    size_of_nodes = (int *)calloc(size, sizeof(int));
     for(int i = 0;i<size;i++)
     {
+        size_of_nodes[i] = 1;
         parent[i] = i;
     }
     count = size;
@@ -18,7 +20,16 @@ void union_f(int a, int b)
     int root_b = find_root(b);
     if(root_a != root_b)
     {
-        parent[root_b] = root_a;
+        //把小的树接到大的树后面，防止单链表型的极端树出现，保证树的大致平衡
+        if(size_of_nodes[root_a] <= size_of_nodes[root_b])
+        {
+            parent[root_b] = root_a;
+            size_of_nodes[root_b] += size_of_nodes[root_a];
+        }else
+        {
+            parent[root_a] = root_b;
+            size_of_nodes[root_a] += size_of_nodes[root_b];
+        }
         count--;
     }
 }
@@ -26,6 +37,8 @@ int find_root(int a)
 {
     while(parent[a]!=a)
     {
+        //缩短路径
+        parent[a] = parent[parent[a]];
         a = parent[a];
     }
     return a;
--- union-find/union_find.h
+++ union-find/union_find.h
@@ -1,15 +1,22 @@
 #ifndef _UNION_FIND_H
 #define _UNION_FIND_H
 
+//记录每个结点的父结点
 int *parent;
 //记录连通分量
 int count;
+//记录每棵树的重量，即结点数
+int *size_of_nodes;
 
+//初始化并查集，所有结点全部独立
 void union_init(int);
+//连通两个结点
 void union_f(int, int);
 //返回root
 int find_root(int);
+//查看两个结点是否连通
 int connected(int, int);
+//查看根节点数,即连通分量
 int getCount();
 
 #endif
