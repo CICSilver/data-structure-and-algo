ddd8d710c4c49ed47a03ab94f4e5f48812493156
root <root@localhost.localdomain>
2020-12-20 02:44:54 -0500

    update myString

---
+++ My_string_cpp/MyString.cc
@@ -0,0 +1,214 @@
+#include "MyString.h"
+
+std::ostream &operator<<(std::ostream &os, const MyString &str)
+{
+    return std::__ostream_insert(os, str.Data(), str.Size());
+}
+
+bool MyString::CheckCapacity(size_t _size)
+{
+    return _size >= Capacity();
+}
+
+// 重新分配后指针地址可能改变，引用/指针不安全
+char *MyString::Grow(size_t minCapacity)
+{
+    size_t oldCapacity = Capacity();
+    size_t newCapacity = oldCapacity + (oldCapacity >> 1);
+    if (minCapacity <= 1)
+    {
+        minCapacity = DEFAULT_SIZE;
+    }
+    if (newCapacity < minCapacity)
+    {
+        newCapacity = minCapacity;
+    }
+
+    str_ = (char *)realloc(str_, sizeof(char) * newCapacity);
+    capacity_ = newCapacity;
+
+    return str_;
+}
+
+MyString MyString::Append(const char *s)
+{
+    size_t s_len = strlen(s);
+    EnsureGrow(s_len + Capacity());
+    memcpy(this->str_ + Size(), s, s_len);
+
+    return *this;
+}
+
+MyString MyString::Append(const char s)
+{
+    EnsureGrow(1 + Capacity());
+    *(this->str_ + Size()) = s;
+
+    return *this;
+}
+
+void MyString::Clear()
+{
+    capacity_ = DEFAULT_SIZE;
+    memset(this->str_, 0, Size());
+    this->str_ = (char *)realloc(this->str_, sizeof(char) * capacity_);
+}
+
+MyString MyString::SubStr(size_t pos, size_t len)
+{
+    MyString temp;
+    CheckPosValid(pos);
+    for (int i = 0; i < len; ++i)
+    {
+        temp.Append(*(this->str_ + pos + i));
+    }
+    return temp;
+}
+
+MyString &MyString::Insert(size_t pos, const char *c)
+{
+    CheckPosValid(pos);
+    size_t c_len = strlen(c);
+    size_t newLen = c_len + this->Size() + 1;
+    EnsureGrow(newLen);
+    char s_temp[newLen] = {0};
+
+    // printf("1");
+    memcpy(s_temp, this->str_, pos);
+    memcpy(s_temp + pos, c, c_len);
+    memcpy(s_temp + pos + c_len, this->str_ + pos, this->Size() - pos);
+    s_temp[newLen] = '\0';
+    memcpy(this->str_, s_temp, newLen + 1);
+
+    return *this;
+}
+
+MyString &MyString::Insert(size_t pos, size_t len, char c)
+{
+    CheckPosValid(pos);
+    char s_temp[len + 1] = {0};
+    for (size_t i = 0; i < len; ++i)
+    {
+        s_temp[i] = c;
+    }
+    s_temp[len] = '\0';
+    return Insert(pos, s_temp);
+}
+
+void MyString::PushBack(char c)
+{
+    this->Append(c);
+}
+
+size_t MyString::FindSubStr(const char *s, size_t spos, size_t epos) const
+{
+    size_t res_pos = npos;
+    bool isDone = false;
+    for (size_t i = spos; i < epos; ++i)
+    {
+        for (size_t j = 0; j < strlen(s); ++j)
+        {
+            if (this->str_[i + j] != s[j])
+            {
+                isDone = false;
+                break;
+            }
+            isDone = true;
+        }
+        if (isDone)
+        {
+            res_pos = i;
+            break;
+        }
+    }
+
+    return res_pos;
+}
+
+size_t MyString::RFindSubStr(const char *s, size_t spos, size_t epos) const
+{
+    size_t res_pos = npos;
+    bool isDone = false;
+    if (spos >= Size())
+    {
+        spos = Size() - 1;
+    }
+    for (size_t i = spos - strlen(s); i >= 0; --i)
+    {
+        for (size_t j = 0; j < strlen(s); ++j)
+        {
+            if (this->str_[i + j] != s[j])
+            {
+                isDone = false;
+                break;
+            }
+            isDone = true;
+        }
+        if (isDone)
+        {
+            res_pos = i;
+            break;
+        }
+    }
+
+    return res_pos;
+}
+
+size_t MyString::Find(const char *s, size_t pos) const
+{
+    return FindSubStr(s, pos);
+}
+
+size_t MyString::FindNotSubStr(const char *s, size_t spos, size_t epos) const
+{
+    size_t res_pos = npos;
+    auto isDone = false;
+    for (size_t i = spos; i < epos; ++i)
+    {
+        for (size_t j = 0; j < strlen(s); ++j)
+        {
+            if (this->str_[i] == s[j])
+            {
+                isDone = false;
+                break;
+            }
+            isDone = true;
+        }
+        if (isDone)
+        {
+            res_pos = i;
+            break;
+        }
+    }
+
+    return res_pos;
+}
+size_t MyString::RFindNotSubStr(const char *s, size_t spos, size_t epos) const
+{
+    size_t res_pos = npos;
+    bool isDone = false;
+
+    if (spos >= Size())
+    {
+        spos = Size() - 1;
+    }
+    for (size_t i = spos; i >= epos; --i)
+    {
+        for (size_t j = 0; j < strlen(s); ++j)
+        {
+            if (this->str_[i] == s[j])
+            {
+                isDone = false;
+                break;
+            }
+            isDone = true;
+        }
+        if (isDone)
+        {
+            res_pos = i;
+            break;
+        }
+    }
+
+    return res_pos;
+}
--- My_string_cpp/MyString.h
+++ My_string_cpp/MyString.h
@@ -4,77 +4,197 @@
 #include <iostream>
 #include <string.h>
 #include <algorithm>
+#include <stdlib.h>
 
 class MyString
 {
 public:
     MyString() : MyString(DEFAULT_SIZE) {}
     explicit MyString(const char *src) : MyString(src, strlen(src)) {}
-    explicit MyString(int size) : capacity_(size)
+    explicit MyString(size_t size) : capacity_(size)
     {
-        if (DEFAULT_SIZE >= size)
+        if (DEFAULT_SIZE >= capacity_)
         {
-            size = DEFAULT_SIZE;
+            capacity_ = DEFAULT_SIZE;
         }
-        str_ = new char[size];
+        str_ = new char[capacity_];
     }
-    MyString(const char *src, int len) : MyString(len)
+    MyString(const char *src, size_t len) : MyString(len)
     {
         strcpy(str_, src);
     }
-    MyString(MyString &string)
+
+    MyString(MyString &string) : str_(nullptr), capacity_(0)
     {
         if (this != &string)
         {
             MyString temp(string.str_);
             std::swap(this->str_, temp.str_);
+            std::swap(this->capacity_, temp.capacity_);
         }
     }
+
     MyString(MyString &&string)
     {
         if (this != &string)
         {
             this->capacity_ = string.capacity_;
-            this->DEFAULT_SIZE = string.DEFAULT_SIZE;
             this->str_ = string.str_;
             string.str_ = nullptr;
         }
     }
-    ~MyString();
+    ~MyString()
+    {
+        delete[] str_;
+    };
 
-    size_t find() const;
-    size_t rfind() const;
-    size_t find_not_of() const;
-    size_t find_last_not_of() const;
+    MyString &operator=(MyString &src)
+    {
+        if (this != &src)
+        {
+            MyString temp(src.str_);
+            std::swap(this->str_, temp.str_);
+            std::swap(this->capacity_, temp.capacity_);
+        }
+        return *this;
+    }
 
-    MyString &Insert(size_t pos, char *c);
+    // TODO
+    MyString operator+=(const MyString &s);
+    MyString operator+=(const char *s);
+
+    friend std::ostream &operator<<(MyString &os, const MyString &str);
+
+    MyString operator++() = delete;
+
+    size_t Find(const char c, size_t pos = 0UL) const
+    {
+        const char *s = &c;
+        return FindSubStr(s, pos);
+    }
+    size_t Find(const char *s, size_t pos = 0UL) const;
+    size_t RFind(const char c, size_t pos = 18446744073709551615UL) const
+    {
+        const char *s = &c;
+        return RFindSubStr(s, pos);
+    }
+    size_t RFind(const char *s, size_t pos = 18446744073709551615UL) const
+    {
+        return RFindSubStr(s, pos);
+    }
+
+    size_t Find_first_not_of(const char c, size_t pos = 0UL) const
+    {
+        const char *s = &c;
+        return FindNotSubStr(s, pos);
+    }
+    size_t Find_first_not_of(const char *s, size_t pos = 0UL) const
+    {
+        return FindNotSubStr(s, pos);
+    }
+
+    size_t Find_last_not_of(const char c, size_t pos = 18446744073709551615UL) const
+    {
+        const char *s = &c;
+        return RFindNotSubStr(s, pos);
+    }
+    size_t Find_last_not_of(const char *s, size_t pos = 18446744073709551615UL) const
+    {
+        return RFindNotSubStr(s, pos);
+    }
+
+    MyString &Insert(size_t pos, const char *c);
     MyString &Insert(size_t pos, size_t len, char c);
 
-    void PopBack();
+    void PopBack()
+    {
+        this->str_[Size() - 1] = '\0';
+    }
     void PushBack(char c);
 
     void Clear();
     MyString Append(const char *s);
 
+    MyString Append(const char s);
+
+    constexpr char At(size_t pos) const;
+
+    bool Empty()
+    {
+        return Size() == 0;
+    }
+
+    MyString &Replace(size_t pos, size_t len, MyString &str);
+
     MyString SubStr(size_t pos = 0UL, size_t len = 18446744073709551615UL);
+
+    void Reserve(size_t newCapacity)
+    {
+        if (newCapacity <= Size())
+        {
+            std::__throw_out_of_range_fmt(__N("capacity (which is %zu) < this->Size() (which is %zu)"), newCapacity, this->Size());
+        }
+        this->str_ = (char *)realloc(this->str_, newCapacity);
+        capacity_ = newCapacity;
+    }
 
     size_t Length()
     {
         return strlen(str_);
     }
 
+    size_t Capacity()
+    {
+        return capacity_ - 1;
+    }
+
+    const char *Data() const
+    {
+        return str_;
+    }
+
+    size_t Size() const
+    {
+        return strlen(str_);
+    }
+
+    size_t Length() const
+    {
+        return strlen(str_);
+    }
+
 private:
     char *str_;
-    int DEFAULT_SIZE = 15;
-    int capacity_;
+    const size_t DEFAULT_SIZE = 15;
+    size_t capacity_;
+
+    const size_t npos = 18446744073709551615UL;
+
+    void EnsureGrow(size_t _size)
+    {
+        auto temp = str_;
+        if (CheckCapacity(_size))
+        {
+
+            temp = Grow(_size);
+        }
+
+        str_ = temp;
+    }
+    bool CheckCapacity(size_t _size);
+    char *Grow(size_t minCapacity);
+    void CheckPosValid(size_t pos)
+    {
+        if (pos > Size())
+        {
+            std::__throw_out_of_range_fmt(__N("pos (which is %zu) > this->Size() (which is %zu)"), pos, this->Size());
+        }
+    }
+    // ANCHOR define.
+    size_t FindSubStr(const char *s, size_t spos = 0UL, size_t epos = 18446744073709551615UL) const;
+    size_t RFindSubStr(const char *s, size_t spos = 18446744073709551615UL, size_t epos = 0UL) const;
+    size_t FindNotSubStr(const char *s, size_t spos = 0UL, size_t epos = 18446744073709551615UL) const;
+    size_t RFindNotSubStr(const char *s, size_t spos = 18446744073709551615UL, size_t epos = 0UL) const;
 };
-
-MyString::MyString(/* args */)
-{
-}
-
-MyString::~MyString()
-{
-}
 
 #endif
